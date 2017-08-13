b67e5ee9c65d78a34c27dc8659e7f5b0da15fff7
Roxanne Charchian <rmcharchian@gmail.com>
2017-08-12 22:05:43 -0500

    server.js completed and task.js router added.  No errors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-//var task = require ('./routes/task')
+var task = require ('./routes/task')
 
 var app = express();
 
@@ -9,7 +9,7 @@ var port = 5000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-//app.use('/task', task)
+app.use('/task', task)
 
 app.listen(port, function() {
     console.log('listening on port', port);
